9ff3a98cf139c7484cd89abc1eb47b0ed5f2c5bd
Tanner C. Myers <tannercmyers@gmail.com>
2021-04-25 11:00:17 -0500

    Adding solution that allows for area calculation when given 1 or 2 arguments

--- area_of_rectangle.py
+++ area_of_rectangle.py
@@ -26,9 +26,9 @@ def area_of_rectangle(height, width = None):
     >>> area_of_rectangle (7, 2)
     14
     """
-    if width:
+    if width is None:
         width = height
-    import pdb; pdb.set_trace()
+    #import pdb; pdb.set_trace()
     area = height * width
     return area
 
@@ -39,10 +39,12 @@ if __name__ == '__main__':
                 "\tthe height of a square or the height and width of a "
                 "rectangle".format(script_name = sys.argv[0]))
         sys.exit(message)
-    height = int(sys.argv[1])
-    width = int(sys.argv[2])
+    if (len(sys.argv) == 3):
+        height = int(sys.argv[1])
+        width = int(sys.argv[2])
     if len(sys.argv) < 3:
-        width = sys.argv[1]
+        width = int(sys.argv[1])
+        height = width
 
     area = area_of_rectangle(height, width)
 
